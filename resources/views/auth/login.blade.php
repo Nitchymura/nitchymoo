12c178a1b50360dd9d9155b944b7ad3be9ed68c3
Takeshi <nitchy_mura_tkc@yahoo.co.jp>
2025-08-27 21:58:40 +0900

    button size of login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,7 +42,7 @@
                         </div>
 
                         <div class="row mb-0">
-                            <div class="col-md-8 col-sm-8 offset-md-4">
+                            <div class="col-md-6 col-sm-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary w-100">
                                     {{ __('Login') }}
                                 </button>
