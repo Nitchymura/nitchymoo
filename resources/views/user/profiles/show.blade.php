f4af358a0c6f9b6011a55d65f83590e729ce4f41
Takeshi <nitchy_mura_tkc@yahoo.co.jp>
2025-09-05 13:29:02 +0900

    profile show

--- resources/views/user/profiles/show.blade.php
+++ resources/views/user/profiles/show.blade.php
@@ -8,7 +8,7 @@
     <div class="row ">
         @forelse($user->posts->sortByDesc('term_start') as $post)
             <div class="col-lg-4 col-md-6 mb-4">
-                <a href="{{ route('post.show', $post->id) }}"><img src="{{ $post->image }}" alt="" class="img-lg d-block mx-auto"></a>
+                <a href="{{ route('post.show', $post->id) }}"><img src="#" alt="{{ $post->title }}" class="img-lg d-block mx-auto"></a>
                 <a href="{{ route('post.show', $post->id) }}" >
                     {{ $post->title }}
                 </a>
