8efe2fbaf56d68038ad9428e7b50912599fdcb07
Takeshi <nitchy_mura_tkc@yahoo.co.jp>
2025-09-05 13:13:11 +0900

    profile show

--- resources/views/user/profiles/show.blade.php
+++ resources/views/user/profiles/show.blade.php
@@ -8,9 +8,9 @@
     <div class="row ">
         @forelse($user->posts->sortByDesc('term_start') as $post)
             <div class="col-lg-4 col-md-6 mb-4">
-                <a href="#" >
+                {{-- <a href="{{ route('post.show', $post->id) }}" > --}}
                     <img src="{{ $post->image }}" alt="" class="grid-img">
-                </a>
+                {{-- </a> --}}
             </div>
         @empty
             <h4 class="h5 text-center text-secondary">No posts yet.</h4>
